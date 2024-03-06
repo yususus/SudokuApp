36a2436a4dc24fe2bbc7c41931a54e8a474438f3
Yusuf Aydın <yusayd3825@gmail.com>
2024-03-06 15:18:02 +0300

    first

---
+++ SudokuApp/SudokuApp/SudokuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuAppApp.swift
+//  SudokuApp
+//
+//  Created by yusuf on 6.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SudokuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
